c5f1d05fff4acf3007546e45c93b25e3d490c77d
Andreas Guldstrand <andreas.guldstrand@gmail.com>
2018-02-08 23:25:46 +0100

    MPTM: Make it a mojo plugin and register table helper

--- lib/Mojolicious/Plugin/TableMaker.pm
+++ lib/Mojolicious/Plugin/TableMaker.pm
@@ -2,4 +2,16 @@ use strict;
 use warnings;
 package Mojolicious::Plugin::TableMaker;
 
+use Mojo::Base 'Mojolicious::Plugin';
+
+sub register {
+  my ($self, $app, $conf) = @_;
+
+  $app->helper(
+    table => sub {
+      return Mojo::HTML::Table->new(@_);
+    }
+  );
+}
+
 1;
